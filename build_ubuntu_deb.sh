517da6c88d2c39d6d13dd337421d068e1b6344c7
oyoung <yangjianhua@iplusbot.com>
2019-10-22 08:50:20 +0000

    add build_ubuntu_deb.sh to make deb package

---
+++ build_ubuntu_deb.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+version=$1
+version=${version:-1.0.0}
+mkdir -p install/usr/local
+mkdir -p install/DEBIAN
+
+cd install/DEBIAN
+
+(cat << EOF
+Package: libwebsockets
+Version: ${version}
+Section: X11
+Priority: optional
+Depends: libssl-dev, openssl
+Architecture: amd64
+Maintainer: oyoung
+CopyRight: community
+Provider: Jeans Oyoung
+Description: WebSocket C Library
+EOF
+) > control
+
+(cat << EOF
+#!/bin/bash
+EOF
+) > preinst
+(cat << EOF
+#!/bin/bash
+EOF
+) > postinst
+(cat << EOF
+#!/bin/bash
+EOF
+) > prerm
+(cat << EOF
+#!/bin/bash
+EOF
+) > postrm
+chmod +x preinst postinst prerm postrm
+
+cd ../..
+
+cmake -Bbuild -H. -DCMAKE_INSTALL_PREFIX=install/usr/local
+cmake --build build --target install
+
+sed 's|^prefix=".*"|prefix="/usr/local"|' -i install/usr/local/lib/pkgconfig/libwebsockets.pc
+sed 's|^prefix=".*"|prefix="/usr/local"|' -i install/usr/local/lib/pkgconfig/libwebsockets_static.pc
+
+fakeroot dpkg -b install libwebsockets_${version}_amd64.deb || exit 1
+rm -rf install
